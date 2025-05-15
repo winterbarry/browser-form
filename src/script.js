fe57d24ae6f8025d8952421db98a7328e8d42c06
Obe Ajilima <ajilimaobe@gmail.com>
2025-05-15 13:13:28 +0100

    added email validation

--- src/script.js
+++ src/script.js
@@ -8,12 +8,30 @@ const postalInput = document.getElementById('postal');
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('confirm-password');
 
+function validateEmail() {
+  const constraints = {
+    email: {
+      presence: { allowEmpty: false, message: "is required" },
+      email: {
+        message: "is not a valid email address"
+      }
+    }
+  };
+
+  const result = validate({ email: emailInput.value }, constraints);
+
+  if (result) {
+    alert(result.email[0]); // show first error encountered
+  } else {
+    alert("Email is valid!");
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
-    validateEmailInput();
+    validateEmail();
   });
 });
